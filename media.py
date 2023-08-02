1b0e2971e921e0a53b61e5426d7a729f978e95a9
Rafhael1901 <rafhaelcorrea1901@gmail.com>
2023-08-02 20:31:34 -0300

    algoritmo para media

---
+++ media.py
@@ -0,0 +1,5 @@
+num1 = float(input('Escreva um primeiro numero: '))
+num2 = float(input('Escreva um segundo numero: '))
+
+media = (num1 + num2)/2
+print (f'Sua media entre ambos os numeros é {media}')
